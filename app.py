968f7fd7f2ae6a3d4dc4fb20cffcdea3583750d1
Adps75 <dps.adrien@gmail.com>
2024-12-29 19:00:57 +1100

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def save_annotation():
     data = request.json
     image_url = data.get("image_url")
     annotations = data.get("annotations", [])  # Liste des annotations sous forme de points
-    bubble_save_url = data.get("bubble_save_url")
+    bubble_save_url = "https://gardenmasteria.bubbleapps.io/version-test/api/1.1/wf/receive_annotations"
 
     if not image_url or not bubble_save_url:
         return jsonify({"success": False, "message": "Paramètres manquants : image_url ou bubble_save_url absent."}), 400
